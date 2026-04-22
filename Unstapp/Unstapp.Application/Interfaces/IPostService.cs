0bbafd63032814b5e812a990d3c071a83b47af50
Santiago Brahim <brahimsantiago04@gmail.com>
2026-04-22 00:15:07 -0300

    feat(Application/Interfaces/*): add auth and post services interfaces

---
+++ Unstapp/Unstapp.Application/Interfaces/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unstapp.Application.DTOs;
+
+namespace Unstapp.Application.Interfaces
+{
+    public interface IPostService
+    {
+        Task<PostDto> CreateAsync(CreatePostDto dto);
+        Task<List<PostDto>> GetAllAsync();
+    }
+}
