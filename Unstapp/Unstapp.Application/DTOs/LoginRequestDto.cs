61c0471b6914e708962a7094e78530ed52792453
Santiago Brahim <brahimsantiago04@gmail.com>
2026-04-22 00:10:57 -0300

    feat(Application/DTOs/*): add DTOs for login and posts

---
+++ Unstapp/Unstapp.Application/DTOs/LoginRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unstapp.Application.DTOs
+{
+    public class LoginRequestDto
+    {
+        public string Dni { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ Unstapp/Unstapp.Application/DTOs/PostDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unstapp.Application.DTOs
+{
+    public class PostDto
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public DateTime PostDate { get; set; }
+        public int LikesCount { get; set; }
+        public int CommentsCount { get; set; }
+    }
+}
