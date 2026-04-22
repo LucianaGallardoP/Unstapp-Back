5f171ae02e8d8bb51bfefa271e06a7f8318b6086
Santiago Brahim <brahimsantiago04@gmail.com>
2026-04-22 13:49:16 -0300

    refactor(API/Controllers/AuthController.cs): add login error message for wrong credentials

--- Unstapp/Unstapp.API/Controllers/AuthController.cs
+++ Unstapp/Unstapp.API/Controllers/AuthController.cs
@@ -22,7 +22,7 @@ namespace Unstapp.API.Controllers
 
             if (result == null)
             {
-                return Unauthorized();
+                return Unauthorized("Usuario o Contraseña Incorrectos");
             }
 
             return Ok(result);
