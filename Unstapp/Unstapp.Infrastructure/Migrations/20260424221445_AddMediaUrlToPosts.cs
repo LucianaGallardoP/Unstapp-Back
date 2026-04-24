3c7ce9dc4c7bc0e7dfbc6343eb2cbec422ee55ac
Santiago Brahim <brahimsantiago04@gmail.com>
2026-04-24 19:39:03 -0300

    feat(migrations): add new migrations for posts wit MediaUrl

---
+++ Unstapp/Unstapp.Infrastructure/Migrations/20260424221445_AddMediaUrlToPosts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unstapp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMediaUrlToPosts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PostDate",
+                table: "Posts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MediaUrl",
+                table: "Posts",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MediaUrl",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PostDate",
+                table: "Posts",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+    }
+}
