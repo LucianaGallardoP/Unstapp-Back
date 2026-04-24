d8b930cccc7115d92298372a5291000d7fdfec10
Santiago Brahim <brahimsantiago04@gmail.com>
2026-04-24 20:41:26 -0300

    feat(Application/DTOs/CreatePostDto.cs): add post validation

--- Unstapp/Unstapp.Application/DTOs/CreatePostDto.cs
+++ Unstapp/Unstapp.Application/DTOs/CreatePostDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,11 @@ namespace Unstapp.Application.DTOs
     public class CreatePostDto
     {
         public int? SubjectId { get; set; }
+
+        [Required(ErrorMessage = "El contenido del post no puede estar vacío.")]
         public string Content { get; set; } = null!;
+
+        [Url(ErrorMessage = "El enlace multimedia no es válido.")]
         public string? MediaUrl { get; set; }
     }
 }
