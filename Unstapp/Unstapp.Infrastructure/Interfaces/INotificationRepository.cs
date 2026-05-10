a27787288e775cfbdfe49951a3bcb21f1a9b9ad3
Santiago Brahim <brahimsantiago04@gmail.com>
2026-05-10 13:24:57 -0300

    feat(Notification Repository): add notification repository

---
+++ Unstapp/Unstapp.Infrastructure/Interfaces/INotificationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unstapp.Infrastructure.Entities;
+
+namespace Unstapp.Infrastructure.Interfaces
+{
+    internal interface INotificationRepository
+    {
+        Task AddAsync(Notification notification);
+        Task<List<Notification>> GetAllByUserIdAsync(int userId);
+        Task<Notification?> GetByIdAsync(int notificationId);
+        Task SaveChangesAsync();
+    }
+}
