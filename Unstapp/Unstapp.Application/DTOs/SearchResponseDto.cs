578b2f282abb4f0c2aaab0b40f75654d7d769750
Santiago Brahim <brahimsantiago04@gmail.com>
2026-05-10 15:38:18 -0300

    feat(Search Service): add search service

--- Unstapp/Unstapp.Application/DTOs/SearchResponseDto.cs
+++ Unstapp/Unstapp.Application/DTOs/SearchResponseDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Unstapp.Application.DTOs
 {
-    internal class SearchResponseDto
+    public class SearchResponseDto
     {
         public List<UserSearchResponseDto> Users { get; set; } = new();
         public List<PostDto> Posts { get; set; } = new();
---
+++ Unstapp/Unstapp.Application/Interfaces/ISearchService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unstapp.Application.DTOs;
+using Unstapp.Shared.DTOs.Common;
+
+namespace Unstapp.Application.Interfaces
+{
+    public interface ISearchService
+    {
+        Task<ServiceResult<SearchResponseDto>> SearchAsync(string term, int userId);
+    }
+}
