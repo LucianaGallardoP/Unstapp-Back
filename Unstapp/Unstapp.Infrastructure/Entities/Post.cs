1a0880570d7ea308916efcf14ab7d6e3c9f997a7
Santiago Brahim <brahimsantiago04@gmail.com>
2026-08-05 19:58:10 -0300

    chore(Database Model): remove SubjectId column from Posts table in database

--- Unstapp/Unstapp.Infrastructure/Entities/Post.cs
+++ Unstapp/Unstapp.Infrastructure/Entities/Post.cs
@@ -11,7 +11,6 @@ namespace Unstapp.Infrastructure.Entities
     {
         public int PostId { get; set; }
         public int UserId { get; set; }
-        public int? SubjectId { get; set; }
         public string? Content { get; set; }
         public string? MediaUrl { get; set; }
         public DateTime PostDate { get; set; } = DateTime.UtcNow;
---
+++ Unstapp/Unstapp.Infrastructure/Migrations/20260805225632_RemoveSubjectIdFromPostEntityModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Unstapp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveSubjectIdFromPostEntityModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubjectId",
+                table: "Posts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SubjectId",
+                table: "Posts",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
